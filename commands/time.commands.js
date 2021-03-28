00d1859ebb0685f8301a56f6e246b6625cbf105d
bird2008 <80196280+bird2008@users.noreply.github.com>
2021-03-28 17:03:48 +0200

    add time

--- commands/time.commands.js
+++ commands/time.commands.js
@@ -1,8 +1,16 @@
+const moment = require('moment');
+const tz = require('moment-timezone');
+const Discord = require('discord.js');
+
+const { timezone, format,} = require(__dirname + "/../config/config.js");
+
+const timeNow = moment().tz(timezone).format(format);
+
 module.exports = {
     name: "czas",
     description: "displays time",
 
     run(msg, args) {
-        msg.reply(new Date().toLocaleTimeString());
+        msg.reply("Jest " + timeNow);
  }
 }
--- config/config.js
+++ config/config.js
@@ -3,4 +3,6 @@ const dotenv = require("dotenv").config()
 module.exports = {
     token: process.env.token,
     prefix: "!",
+    timezone: 'Europe/Poland',
+    format: 'HH:mm',
 }
